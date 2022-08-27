853fd9e01d643aa376327f5040ea6749c73fa961
Max <lomanuy@gmail.com>
2022-08-27 20:16:13 +0300

    сделал 2 задание

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿while(true)
+{
+    Console.WriteLine("Введите число: ");
+    int a = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите число: ");
+    int b = int.Parse(Console.ReadLine());
+
+    if (a > b)
+    {
+        Console.WriteLine($"Число {a} больше числа {b}.");
+    }
+
+    else
+    {
+        Console.WriteLine($"Число {a} меньше числа {b}.");
+    }
+
+}
